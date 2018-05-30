fab475707b3c1abb9e4ede17eb02267833edea24
League <league@WTS-IM-17.attlocal.net>
2018-05-29 18:25:40 -0700

    goofy names, guessing pi IP

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,3 +1,5 @@
+
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -7,29 +9,32 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "";
-
+		String goofyName = ""; 
 		// 1. Ask the user to enter their name
-String i = JOptionPane.showInputDialog("what is your name in lower case");
+		String i = JOptionPane.showInputDialog("what is your name in lower case");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-System.out.println(i.toUpperCase());
+		// Run your program to see that this works.
+		System.out.println(i.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-for(int s = 0;s<i.length();s++) {
-	
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at s
-			System.out.println(i.substring(s,1+s));
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-if() {
-	
-}
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-}
-		// 7. Use pop-up to show user their Goofy name
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int s = 0; s < i.length(); s++) {
 
+			// 4. Create a String variable to store the next character of the name
+			// using .substring(start, end)
+			// HINT: replace 'start' and 'end' to get String with character at s
+			String sam = i.substring(s, 1 + s);
+			// 5. Use MODULO to set this variable to upper case for EVEN characters
+			// and lower case for ODD characters.
+			if (s % 2 == 0) {
+				sam = sam.toUpperCase();
+			} else {
+				sam = sam.toLowerCase();
+			}
+			// 6. ADD this String (containing 1 char) to the goofyName String
+		goofyName = goofyName + sam;
+		
+		}
+		// 7. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null, "your goofy name is "+goofyName);
 	}
 }
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -7,23 +7,25 @@ import javax.swing.JOptionPane;
 
 public class GuessingPi {
 	// 1. Make a main method and make sure your program can run
-
+public static void main(String[] args) {
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-
+String pi = "3.1415926535";
 		// 3. Print out the first 3 digits of Pi to the console.
 		//    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-
+System.out.print(pi.charAt(0));
+System.out.print(pi.charAt(1));
+System.out.print(pi.charAt(2));
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
 
 			// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
-
+for (int i = 0; i < args.length; i++) {
 				// 5. Ask the user for the NEXT digit of pi.
-
+String sam = 
 				// 6. Compare the user's input to the next digit of your pi variable 
 				//    (look at step 3 for a clue).
 				//    HINT: use charAt(0) to get 1st char of user input String 
@@ -33,6 +35,8 @@ public class GuessingPi {
 					// 8. If they are not, print out "incorrect" and tell them
 					//    to start over. Use 'break;' to break out of the loop.
 
+}
+}
 }
 
 
