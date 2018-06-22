d4cc8a50093b8a640d5dd7142d3ae22a66ceb40f
Samson <kempiak@gmail.com>
2018-06-22 16:58:44 -0700

    for loop gauntlet ip

---
+++ src/extra/Flg.java
@@ -0,0 +1,42 @@
+package extra;
+
+public class Flg {
+public static void main(String[] args) {
+/*
+	for (int i = 0; i < 101; i++) {
+	System.out.println(i);
+}	
+for (int i = 100; i > -1; i--) {
+	System.out.println(i);
+}
+
+for (int i = 2; i < 101; i++) {
+	System.out.println(i);
+}
+
+	for (int i = 1; i < 100; i++) {
+		System.out.println(i);
+	}
+
+	for (int i = 1; i < 501; i++) {
+		if (i%2==0) {
+			System.out.println(i+" is even");
+		}else {
+			System.out.println(i+" is odd");
+		}
+	}
+for (int i = 07; i < 778; i+=7) {
+	System.out.println(i);
+}
+
+for (int i = 2006; i < 2018; i++) {
+	System.out.println(i+" i am "+(i-2006));
+}
+*/
+for (int i = 0; i < 10; i++) {
+	for (int j = 0; j < 10; j++) {
+		
+	}
+}
+}
+}
